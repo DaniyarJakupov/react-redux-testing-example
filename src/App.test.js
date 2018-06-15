a111041b8b57a96b0782945cd58804aaf510afa7
Daniyar Jakupov <ddr199305@gmail.com>
2018-06-15 11:01:24 +0200

    Add beforeEach

--- src/App.test.js
+++ src/App.test.js
@@ -4,16 +4,17 @@ import App from "App";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-it("shows a comment box", () => {
+let wrapped;
+beforeEach(() => {
   // Shallow renders only the given component
   // and none of its React Children. DOM elements are rendered (<h1>, <p>, etc)
-  const wrapped = shallow(<App />);
+  wrapped = shallow(<App />);
+});
 
+it("shows a comment box", () => {
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
