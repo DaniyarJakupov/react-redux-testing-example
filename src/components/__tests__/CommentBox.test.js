bdc978ba499ca39bdaa87ef25dee0e0f719bdd13
Daniyar Jakupov <ddr199305@gmail.com>
2018-06-16 21:41:32 +0200

    Connect CommentBox to redux store in testing env

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { mount } from "enzyme";
 import CommentBox from "components/CommentBox";
+import WithProvider from "redux/index";
 
 let wrapped;
 beforeEach(() => {
-  wrapped = mount(<CommentBox />);
+  wrapped = mount(
+    <WithProvider>
+      <CommentBox />
+    </WithProvider>
+  );
 });
 afterEach(() => {
   wrapped.unmount();
