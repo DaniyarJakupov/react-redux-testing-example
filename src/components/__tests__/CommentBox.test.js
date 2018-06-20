c4a063ccb2d2f3bdf4b6489d5fb638be45a8cb4e
Daniyar Jakupov <ddr199305@gmail.com>
2018-06-20 21:11:47 +0200

    Fix test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,10 +16,10 @@ afterEach(() => {
 });
 
 /* ===========================General tests =========================== */
-it("has a textarea and a button", () => {
+it("has a textarea and two button", () => {
   //Check if CommentBox component has a textarea and a button
   expect(wrapped.find("textarea").length).toEqual(1);
-  expect(wrapped.find("button").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(2);
 });
 
 /* ====================== TextArea tests =============================== */
